694b5b137458c069d07bc1adf7fe42b114f0e70f
Antoine <antoine.montier@sfr.fr>
2023-03-04 21:25:57 +0100

    the algo is now finding a path, but the verification function needs to be fixed

--- simple_solving.c
+++ simple_solving.c
@@ -26,7 +26,7 @@ typedef struct{
 typedef slot* chemin;
 
 laby create_labyrinth(int cols, int lines);
-int is_solution_true(laby, slot*, int);
+int is_solution_true(laby, chemin);
 void free_labyrinth(laby*, int cols, int lines);
 void print_labyrinth(laby , int cols, int lines);
 slot* solve_labyrinth(laby, int cols, int lines);
@@ -34,14 +34,30 @@ void print_slot(slot);
 int rec_find(laby, chemin res, slot start, slot end);
 int slot_in_chemin(int col, int line, chemin c);
 void add_current_coordinates_to_chemin(int col, int line, chemin c);
+void print_chemin(chemin c);
+
 
 int main(){
     laby l = create_labyrinth(COLS, LINES);
     print_labyrinth(l, COLS, LINES);
-    solve_labyrinth(l, COLS, LINES);
+    chemin ans = solve_labyrinth(l, COLS, LINES);
+    if(is_solution_true(l, ans))
+        printf("Solution is true\n");
+    print_chemin(ans);
     free_labyrinth(&l, COLS, LINES);
     return 0;
 }
+
+void print_chemin(chemin c){
+    printf("[");
+    for(int i = 0; i < CHEMIN_LENGTH ; i++){
+        if(c[i].col == -1 && c[i].line == -1)
+            break;
+        printf(" %d;%d ", c[i].col, c[i].line);
+    }
+    printf("]\n");
+}
+
 
 void add_current_coordinates_to_chemin(int col, int line, chemin c){
     for(int i = 0 ; i < CHEMIN_LENGTH; i++)
@@ -63,31 +79,36 @@ int slot_in_chemin(int col, int line, chemin c){
 int rec_find(laby l, chemin res, slot current, slot end){
     if(res == NULL)
         fprintf(stderr, "chemin is null, use malloc\n");
+    if(res[CHEMIN_LENGTH-1].col == -123 && res[CHEMIN_LENGTH-1].line == -123)
+        return 0; // a solution has already been found
     add_current_coordinates_to_chemin(current.col, current.line, res);
+
     if(current.col == end.col && current.line == end.line){
         printf("solved\n");
-        print_slot(current);
+        slot end_signal = {-123, -123};
+        res[CHEMIN_LENGTH-1] = end_signal;
         return 1;
     }
-    print_slot(current);
+
     // check 4 directions
 
-    if(!slot_in_chemin(current.col-1, current.line, res) && l[current.col-1][current.line] != WALL){
+    if(current.col - 1 >= 0 && !slot_in_chemin(current.col-1, current.line, res) && l[current.col-1][current.line] != WALL){
         slot next = {current.col-1, current.line};
         rec_find(l, res, next, end);   
     }
-    if(!slot_in_chemin(current.col+1, current.line, res) && l[current.col+1][current.line] != WALL){
+    if(current.col+1 < COLS && !slot_in_chemin(current.col+1, current.line, res) && l[current.col+1][current.line] != WALL){
         slot next = {current.col+1, current.line};
         rec_find(l, res, next, end);
     }
-    if(!slot_in_chemin(current.col, current.line-1, res) && l[current.col][current.line-1] != WALL){
+    if(current.line-1 >= 0 && !slot_in_chemin(current.col, current.line-1, res) && l[current.col][current.line-1] != WALL){
         slot next = {current.col, current.line-1};
         rec_find(l, res, next, end);
     }
-    if(!slot_in_chemin(current.col, current.line+1, res) && l[current.col][current.line+1] != WALL){
+    if(current.line+1 < LINES && !slot_in_chemin(current.col, current.line+1, res) && l[current.col][current.line+1] != WALL){
         slot next = {current.col, current.line+1};
         rec_find(l, res, next, end);
-    }   
+    }
+    return 0;
 }
 
 
@@ -122,7 +143,7 @@ slot* solve_labyrinth(laby l, int cols, int lines){
         answer[i].line = -1;
     }
     rec_find(l, answer, start, end);
-
+    return answer;
     free(answer);
 }
 
@@ -159,15 +180,21 @@ void print_labyrinth(laby l, int cols, int lines){
 
 
 
-int is_solution_true(laby l, slot*slot_array, int path_length){
+int is_solution_true(laby l, slot*slot_array){
     //check if beginning is entry and end is exit
 
     if(l[slot_array[0].col][slot_array[0].line] != ENTRY)
         return 0; //false
-    if(l[slot_array[path_length - 1].col][slot_array[path_length - 1].line] != EXIT)
-        return 0; //false
+    int index_end;
+    for(index_end = 0; index_end < CHEMIN_LENGTH-1 ; index_end++)
+        if(slot_array[index_end+1].col == -1 && slot_array[index_end+1].line == -1)
+            break;
+    if(index_end == CHEMIN_LENGTH - 1)
+        return 0; // no end to the path
+    //else we just need to check until the end of the path
+
     //check if each slots are neightbours
-    for(int i = 1; i < path_length ; i++){
+    for(int i = 1; i <= index_end ; i++){
         //same col
         if(slot_array[i].col == slot_array[i-1].col){
             //check the line difference between the two lines is 1.
