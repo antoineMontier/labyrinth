67ddbf3414edf8007a3439ed9fcab698de2c9980
Antoine <antoine.montier@sfr.fr>
2023-03-14 14:04:29 +0100

    debugging code

--- simple_solving.c
+++ simple_solving.c
@@ -3,6 +3,7 @@
 int main(){
     Laby l = creer_labyrinth(11, 11);
     chemin ans = solve_labyrinth_threads(l);
+    print_chemin(ans);
     if(check_solution(l, ans))
         printf("Solution OK\n");
     print_solution(l, ans);
